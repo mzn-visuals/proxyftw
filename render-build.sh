ba10c096da84bcc8fdb60db4e5318023f2ea2bb9
mmaznnn <94003574+mzn-visuals@users.noreply.github.com>
2026-08-01 01:33:48 +0400

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Install system dependencies (ffmpeg)
+echo "Installing ffmpeg..."
+apt-get update
+apt-get install -y ffmpeg
+
+# 2. Install Node dependencies
 npm install
 
+# 3. Download latest yt-dlp
 echo "Downloading yt-dlp..."
 mkdir -p ./bin
 wget -O ./bin/yt-dlp https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp
